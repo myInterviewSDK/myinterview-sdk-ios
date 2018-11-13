9b5491d2a6829d0f077c07921ed904b8fe749ed7
Amir Nuriev <amir.nuriev@agileengine.com>
2018-11-13 17:28:30 +0300

    Initial commit

---
+++ MIWidget.framework/Headers/MIWidget.h
@@ -0,0 +1,19 @@
+//
+//  myinterview_sdk.h
+//  myinterview-sdk
+//
+//  Created by Oleksii Kozulin on 6/11/18.
+//  Copyright © 2018 myInterview. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for myinterview_sdk.
+FOUNDATION_EXPORT double myinterview_sdkVersionNumber;
+
+//! Project version string for myinterview_sdk.
+FOUNDATION_EXPORT const unsigned char myinterview_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myinterview_sdk/PublicHeader.h>
+
+
